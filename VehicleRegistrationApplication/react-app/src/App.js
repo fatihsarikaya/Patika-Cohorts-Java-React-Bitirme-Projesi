ac19e696deb0e723d1381bbae1cda1f095cdd06e
Fatih Sarikaya <fatihsarikaya4@hotmail.com>
2023-10-21 22:25:29 +0300

    update

--- VehicleRegistrationApplication/react-app/src/App.js
+++ VehicleRegistrationApplication/react-app/src/App.js
@@ -1,73 +1,27 @@
 import React, { Component } from 'react';
+import SignUp from './SignUp';
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
-class SignUp extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      username: '',
-      email: '',
-      password: ''
-    };
-  }
-
-  handleInputChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const { username, email, password } = this.state;
-
-    // Bu noktada sunucuya kayıt işlemini yapabilirsiniz.
-    // Örnek: fetch veya axios kullanarak bir POST isteği gönderme
-
-    console.log('Username:', username);
-    console.log('E-mail:', email);
-    console.log('Password:', password);
-  }
-
-  render() {
-    return (
-        <div>
-          <h2>Kayıt Ol</h2>
-          <form onSubmit={this.handleSubmit}>
-            <div>
-              <label>Username</label>
-              <input
-                  type="text"
-                  name="username"
-                  value={this.state.username}
-                  onChange={this.handleInputChange}
-                  required
-              />
-            </div>
-            <div>
-              <label>E-mail</label>
-              <input
-                  type="email"
-                  name="email"
-                  value={this.state.email}
-                  onChange={this.handleInputChange}
-                  required
-              />
-            </div>
-            <div>
-              <label>Password</label>
-              <input
-                  type="password"
-                  name="password"
-                  value={this.state.password}
-                  onChange={this.handleInputChange}
-                  required
-              />
-            </div>
-            <button type="submit">Sign Up</button>
-          </form>
-        </div>
-    );
-  }
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <Routes>
+                        <Route path="/signup" element={<SignUp />} />
+                        <Route path="/" element={
+                            <React.Fragment>
+                                <header className="App-header">
+                                    <Link to="/signup">Sign Up</Link>
+                                </header>
+                                {/* Diğer bileşenler veya içerikler buraya eklenebilir */}
+                            </React.Fragment>
+                        } />
+                    </Routes>
+                </div>
+            </Router>
+        );
+    }
 }
 
-export default SignUp;
+export default App;
