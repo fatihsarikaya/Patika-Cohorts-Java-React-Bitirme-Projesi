a8f51d109b7a84e36606e42de610f01398401424
Fatih Sarikaya <fatihsarikaya4@hotmail.com>
2023-10-21 14:31:59 +0300

    İlk-Hali

---
+++ VehicleRegistrationApplication/src/main/java/com/java/VehicleRegistrationApplication/VehicleRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.java.VehicleRegistrationApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRegistrationApplication.class, args);
+	}
+
+}
