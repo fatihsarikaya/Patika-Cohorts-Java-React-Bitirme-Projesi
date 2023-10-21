4ec858fd0006afb7e35cb68d68abd66a158be61e
Fatih Sarikaya <fatihsarikaya4@hotmail.com>
2023-10-21 20:04:05 +0300

    Update App.js

--- VehicleRegistrationApplication/react-app/src/App.js
+++ VehicleRegistrationApplication/react-app/src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class SignUp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      email: '',
+      password: ''
+    };
+  }
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { username, email, password } = this.state;
+
+    // Bu noktada sunucuya kayıt işlemini yapabilirsiniz.
+    // Örnek: fetch veya axios kullanarak bir POST isteği gönderme
+
+    console.log('Username:', username);
+    console.log('E-mail:', email);
+    console.log('Password:', password);
+  }
+
+  render() {
+    return (
+        <div>
+          <h2>Kayıt Ol</h2>
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <label>Username</label>
+              <input
+                  type="text"
+                  name="username"
+                  value={this.state.username}
+                  onChange={this.handleInputChange}
+                  required
+              />
+            </div>
+            <div>
+              <label>E-mail</label>
+              <input
+                  type="email"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.handleInputChange}
+                  required
+              />
+            </div>
+            <div>
+              <label>Password</label>
+              <input
+                  type="password"
+                  name="password"
+                  value={this.state.password}
+                  onChange={this.handleInputChange}
+                  required
+              />
+            </div>
+            <button type="submit">Sign Up</button>
+          </form>
+        </div>
+    );
+  }
 }
 
-export default App;
+export default SignUp;
