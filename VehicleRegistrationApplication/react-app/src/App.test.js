7dae97b24ff4c9cac9ef16fa3cb1dd938445e5ad
Fatih Sarikaya <fatihsarikaya4@hotmail.com>
2023-10-23 22:32:17 +0300

    Update App.test.js

--- VehicleRegistrationApplication/react-app/src/App.test.js
+++ VehicleRegistrationApplication/react-app/src/App.test.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn react/i, { selector: 'a' }); // Specify the <a> tag as the selector
   console.log(screen.debug()); // Add this line for debugging
   expect(linkElement).toBeInTheDocument();
 });
